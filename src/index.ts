15cffe1d84eaa0957be5da74d80720b4aaf0b3b4
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-27 21:34:20 -0300

    created route multiply

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,13 @@ server.get("/dividir", (request, response)=>{
     response.send(`Resultado: ${result.toString()} `);
 });
 
+server.get("/multiplicar", (request, response)=>{
+    const num1 = parseFloat(process.env.NUM_1 || "0");
+    const num2 = parseFloat(process.env.NUM_2 || "0");
+    const result = num1 * num2;
+    response.send(`Resultado: ${result.toString()} `);
+});
+
 server.listen({
     port: process.env.PORT || 8000,
     log: console.log(`Servidor rodando na porta ${process.env.PORT || 8000}`)
