a008277e89484d98738dc1c83453856aa70ce045
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-27 21:33:42 -0300

    created route split

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,12 @@ server.get("/subtrair", (request, response)=>{
     response.send(`Resultado: ${result.toString()} `);
 });
 
+server.get("/dividir", (request, response)=>{
+    const num1 = parseFloat(process.env.NUM_1 || "0");
+    const num2 = parseFloat(process.env.NUM_2 || "0");
+    const result = num1 / num2;
+    response.send(`Resultado: ${result.toString()} `);
+});
 
 server.listen({
     port: process.env.PORT || 8000,
